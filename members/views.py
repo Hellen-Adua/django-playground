8d48356e8c1c6c88407d6df802e0aae27f5b4787
Hellen-Adua <hellenaduda@gmail.com>
2025-08-15 20:54:34 +0300

    Add templates, update views.py

--- members/views.py
+++ members/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world! This is my first Django app.")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
