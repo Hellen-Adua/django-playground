27aacdd543e1cce7eb04da73ba7a43fc2118f87d
Hellen-Adua <hellenaduda@gmail.com>
2025-08-17 18:43:40 +0300

    added templates, made changes to views.py, admin.py, created models

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='members'),
+    path('', views.main, name='main'),
+    path('home/', views.home, name='home'),
+    path('all_members/', views.all_members, name='all_members'),
+    path('all_members/details/<int:id>', views.details, name='details'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('members.urls')),        # Root path
-    path('members/', include('members.urls')) # Also allow /members/
+    # path('members/', include('members.urls')) # Also allow /members/
 ]
